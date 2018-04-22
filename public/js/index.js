26568568d88a388581b5cb519a838a4b5286216d
Kuan Wen <j70915@gmail.com>
2018-04-22 19:43:28 +0800

    basic side-nav and map

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,12 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidenav").style.width = "380px";
+    document.getElementById("mySidenav").style.boxShadow="4px 4px 12px 4px rgba(20%,20%,40%,0.5)";
+    document.getElementById("main").style.marginLeft = "380px";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.boxShadow="none";
     document.getElementById("main").style.marginLeft = "0";
 }
 
@@ -18,6 +20,44 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
     id: 'mapbox.streets'
 }).addTo(mymap);
 
-L.easyButton('fa-globe', function(btn, map){
-    openNav();
+L.easyButton({
+    states: [{
+        stateName: 'open-nav',
+        icon: 'fa-map-marker',
+        title: '展開側欄',
+        onClick: function(control) {
+          openNav();
+          control.state('close-nav');
+        }
+      }, {
+        icon: 'fa-undo',
+        stateName: 'close-nav',
+        onClick: function(control) {
+          closeNav();
+          control.state('open-nav');
+        },
+        title: '縮小側欄'
+      }]
 }).addTo(mymap);
+
+function openTab(evt, tabName) {
+    // Declare all variables
+    console.log(evt)
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
