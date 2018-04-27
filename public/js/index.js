a6207b4623ec4feece466b1305111d8c07f9fb74
Kuan Wen <j70915@gmail.com>
2018-04-27 14:11:37 +0800

    remove / before file path

--- public/js/index.js
+++ public/js/index.js
@@ -10,76 +10,55 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
 }
 
-/*
-function openTab(evt, tabName) {
-    // Declare all variables
-    console.log(evt)
-    var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+$(document).ready(function() {
+    var mymap = L.map('main').setView([23.5971,121.0126], 8);
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+            '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        id: 'mapbox.streets'
+    }).addTo(mymap);
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
-*/
-
-var mymap = L.map('main').setView([23.5971,121.0126], 8);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    id: 'mapbox.streets'
-}).addTo(mymap);
-
-L.easyButton({
-    states: [{
-        stateName: 'open-nav',
-        icon: 'fa-angle-double-right',
-        title: '展開側欄',
-        onClick: function(control) {
-          openNav();
-          control.state('close-nav');
-        }
-      }]
-}).addTo(mymap);
-
-var dataColumn =  ["name","address","supervisor","manager","date","cost","type"]
-
-$('.menu .item').tab();
-$('.ui.dropdown').dropdown('set selected', "0");
-$('.tab[data-tab="info"] .sub.header').hide();
-
-$.getJSON("/data.json", function(data) {
-    data.forEach((place, index) => {
-        L.marker(place.coordinate).addTo(mymap).on('click', function(){
+    L.easyButton({
+        states: [{
+            stateName: 'open-nav',
+            icon: 'fa-angle-double-right',
+            title: '展開側欄',
+            onClick: function(control) {
             openNav();
-            
-            // change to INFO tab
-            $('.ui.menu > .item').removeClass('active');
-            $('.ui.menu > .item[data-tab="info"]').addClass('active');
-            $('.ui.tab').removeClass('active');
-            $('.ui.tab[data-tab="info"]').addClass('active');
+            control.state('close-nav');
+            }
+        }]
+    }).addTo(mymap);
 
-            $('.tab[data-tab="info"] .sub.header').show();
+    var dataColumn =  ["name","address","supervisor","manager","date","cost","type"]
 
-            dataColumn.forEach(function(col){
-                console.log(col);
-                document.getElementById('info-'+col).innerHTML = place[col];
-            })
-            mymap.setView(place.coordinate, 9);
+    $('.menu .item').tab();
+    $('.ui.dropdown').dropdown('set selected', "0");
+    $('.tab[data-tab="info"] .sub.header').hide();
+
+    $.getJSON("data.json", function(data) {
+        data.forEach((place, index) => {
+            L.marker(place.coordinate).addTo(mymap).on('click', function(){
+                openNav();
+                
+                // change to INFO tab
+                $('.ui.menu > .item').removeClass('active');
+                $('.ui.menu > .item[data-tab="info"]').addClass('active');
+                $('.ui.tab').removeClass('active');
+                $('.ui.tab[data-tab="info"]').addClass('active');
+
+                $('.tab[data-tab="info"] .sub.header').show();
+
+                dataColumn.forEach(function(col){
+                    console.log(col);
+                    document.getElementById('info-'+col).innerHTML = place[col];
+                })
+                mymap.setView(place.coordinate, 9);
+            });
         });
-    });
+    })
 })
